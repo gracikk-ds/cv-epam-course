e26d5125dfdb4ef09df1dc6b186618e9212c2c5b
gordeev.al <aleksandr_gordeev2@epam.com>
2022-03-25 12:09:36 +0300

    fix dataset class

--- Module 5. Metric learning/trainer_docker/task.py
+++ Module 5. Metric learning/trainer_docker/task.py
@@ -1,40 +1,21 @@
-import os
-import sys
 import click
-import tempfile
-import traceback
-import contextlib
-import subprocess
+import logging
 from pathlib import Path
-from typing import Tuple
 
 import torch
 from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
 
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_metric_learning.samplers import MPerClassSampler
 
-from .data import Transforms, FilteredImageFolder
+from .data import Transforms
 from .metric_learning import EmbeddingsModel, Runner, BATCH_SIZE
 
 
 SIZE = 224
-
-SELECTED_BRANDS = (
-    "Sochnyj",
-    "Rich",
-    "Schaste",
-    "Agusha",
-    "Sadochok",
-)
 BACKBONE = "resnext101_32x8d"
-
-
-def target_pretransform_fn(target: str, selected_brands: Tuple[str] = SELECTED_BRANDS):
-    if any([target.lower().startswith(prefix.lower()) for prefix in selected_brands]):
-        return target
-    return "other"
 
 
 @click.command()
@@ -48,118 +29,110 @@ def main(
         model_dir: str,
         max_epochs: int,
 ):
+    tb_log_dir_to_use = Path(tb_log_dir)
+    model_dir_to_use = Path(model_dir)
+    dataset_folder_to_use = Path(dataset_folder)
 
-    path = tb_log_dir / "stdout.txt"
-    path_err = tb_log_dir / "stderr.txt"
+    # setting up logger
+    logging.basicConfig(
+        filename=str(tb_log_dir_to_use / "logs.log"),
+        filemode="a",
+        format="%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s",
+        datefmt="%H:%M:%S",
+        level=logging.DEBUG,
+    )
 
-    with open(path, "w") as f, open(path_err, "w") as e:
-        with contextlib.redirect_stdout(f), contextlib.redirect_stderr(e):
+    classes_train = set(
+        [p.name for p in (dataset_folder_to_use / "train").glob("*")]
+    )
+    classes_val = set(
+        [p.name for p in (dataset_folder_to_use / "val").glob("*")]
+    )
 
-            classes_train = set(
-                [p.name for p in (dataset_folder / "train").glob("*")]
-            )
-            classes_val = set(
-                [p.name for p in (dataset_folder / "val").glob("*")]
-            )
+    print(
+        f"Number of classes in train {len(classes_train)}",
+        f"Number of classes in val {len(classes_val)}",
+        f"Number of classes in train & val {len(classes_train & classes_val)}",
+        f"Number of classes in train - val {len(classes_train - classes_val)}",
+    )
 
-            print(
-                f"Number of classes in train {len(classes_train)}",
-                f"Number of classes in val {len(classes_val)}",
-                f"Number of classes in train & val {len(classes_train & classes_val)}",
-                f"Number of classes in train - val {len(classes_train - classes_val)}",
-            )
+    train_dataset = ImageFolder(
+        root=str(dataset_folder_to_use / "train"),
+        transform=Transforms(),
+    )
 
-            classes_superset = sorted(classes_train)
+    val_dataset = ImageFolder(
+        root=str(dataset_folder_to_use / "test"),
+        transform=Transforms(segment="val"),
+    )
 
-            train_dataset = FilteredImageFolder(
-                str(dataset_folder / "train"),
-                transform=Transforms(),
-                classes_superset=classes_superset,
-            )
-            val_dataset = FilteredImageFolder(
-                str(dataset_folder / "val"),
-                classes_superset=classes_superset,
-                transform=Transforms(segment="val"),
-            )
+    sampler = MPerClassSampler(
+        train_dataset.targets,
+        m=3,
+        length_before_new_iter=len(train_dataset),
+    )
 
-            sampler = MPerClassSampler(
-                train_dataset.targets,
-                m=3,
-                length_before_new_iter=len(train_dataset),
-            )
-            train_dl = DataLoader(
-                train_dataset,
-                BATCH_SIZE,
-                pin_memory=False,
-                sampler=sampler,
-                num_workers=8,
-                drop_last=True,
-            )
-            val_dl = DataLoader(
-                val_dataset,
-                BATCH_SIZE,
-                pin_memory=False,
-                shuffle=False,
-                num_workers=4,
-                drop_last=False,
-            )
+    train_dl = DataLoader(
+        train_dataset,
+        BATCH_SIZE,
+        pin_memory=False,
+        sampler=sampler,
+        num_workers=8,
+        drop_last=True,
+    )
 
-            assert val_dataset.classes == train_dataset.classes
+    val_dl = DataLoader(
+        val_dataset,
+        BATCH_SIZE,
+        pin_memory=False,
+        shuffle=False,
+        num_workers=4,
+        drop_last=False,
+    )
 
-            class_mapping = {
-                clz: target_pretransform_fn(clz)
-                if clz in val_dataset.existing_classes
-                else "other"
-                for clz in val_dataset.classes
-            }
+    assert val_dataset.classes == train_dataset.classes
 
-            runner = Runner(
-                model=EmbeddingsModel(
-                    num_classes=len(set(class_mapping.values())), backbone=BACKBONE
-                ),
-                classes=train_dataset.classes,
-                class_mapping=class_mapping,
-                lr=1e-4,
-                scheduler_T=max_epochs * len(train_dl),
-            )
+    runner = Runner(
+        model=EmbeddingsModel(
+            num_classes=len(classes_train),
+            backbone=BACKBONE
+        ),
+        classes=train_dataset.classes,
+        lr=1e-3,
+        scheduler_T=max_epochs * len(train_dl),
+    )
 
-            trainer = pl.Trainer(
-                max_epochs=max_epochs,
-                gpus=-1,
-                # gradient_clip_val=1.0,
-                logger=pl.loggers.TensorBoardLogger(tb_log_dir),
-                callbacks=[
-                    ModelCheckpoint(
-                        dirpath=model_dir,
-                        save_top_k=-1,
-                        verbose=True,
-                        filename="checkpoint-{epoch:02d}",
-                    ),
-                ],
-            )
+    trainer = pl.Trainer(
+        max_epochs=max_epochs,
+        gpus=-1,
+        logger=pl.loggers.TensorBoardLogger(tb_log_dir_to_use),
+        callbacks=[
+            ModelCheckpoint(
+                dirpath=model_dir,
+                save_top_k=-1,
+                verbose=True,
+                filename="checkpoint-{epoch:02d}",
+            ),
+        ],
+    )
 
-            try:
-                trainer.fit(runner, train_dl, val_dl)
+    trainer.fit(runner, train_dl, val_dl)
 
-                runner.model.eval()
-                b = next(iter(val_dl))
-                traced_model = torch.jit.trace(runner.model, b[0])
-                meta = {
-                    "class_names": runner.mapped_classes,
-                    "inference_params": {
-                        "image_height": SIZE,
-                        "image_width": SIZE,
-                    },
-                }
-                traced_model.save(
-                    str(model_dir / "torchscript.pt"),
-                    _extra_files={f"{k}.txt": str(v) for k, v in meta.items()},
-                )
-
-            except Exception as ex:
-                print(ex)
-                print(traceback.format_exc(), flush=True)
-                print(traceback.print_stack(), flush=True)
+    # save the model
+    runner.model.eval()
+    b = next(iter(val_dl))
+    traced_model = torch.jit.trace(runner.model, b[0])
+    meta = {
+        "class_names": runner.mapped_classes,
+        "inference_params": {
+            "image_height": SIZE,
+            "image_width": SIZE,
+        },
+    }
+    traced_model.save(
+        str(model_dir_to_use / "torchscript.pt"),
+        _extra_files={f"{k}.txt": str(v) for k, v in meta.items()},
+    )
 
 
 if __name__ == "__main__":
